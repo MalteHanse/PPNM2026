74047df460942c35b050ea0ec705fedd30a2ab42
MalteHanse <hansenmalte@icloud.com>
2026-01-28 09:13:35 +0100

    The first c++ is worling

--- lectures/fib.cpp
+++ lectures/fib.cpp
@@ -1,1 +1,18 @@
 #include<iostream>
+
+int fib(int n) {
+    if (n < 0) return -1;
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char** argv) {
+    int n = std::stoi(argv[1]);
+    std::printf("n=%i\n", n);
+
+    for (int i = 0; i < n + 1; i++) {
+        std::printf("fib(%i)=%i\n", i, fib(i));
+    }
+    return 0;
+}
